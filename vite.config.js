df4b5bb7ee6c9ec47233bc42e140910003e9261e
charleston1107 <charleston1887@gmail.com>
2026-04-23 01:12:32 +0800

    add all html files to build output

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { globSync } from 'glob'
 
+const pages = Object.fromEntries(
+  globSync('**/*.html', { ignore: ['node_modules/**', 'dist/**'] })
+    .map(file => [
+      file.replace(/\.html$/, ''), 
+      resolve(__dirname, file)
+    ])
+)
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: pages
+    }
+  },
   base: "/Gathering-the-Cracks"
 })
